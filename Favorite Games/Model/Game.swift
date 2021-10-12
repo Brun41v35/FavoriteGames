8fae24b6b20ea624543fea0bd52ed0b68cf17600
Brun41v35 <alves.bruno945@gmail.com>
2021-10-12 20:28:14 -0300

    Finishing the project with custom cell

---
+++ Favorite Games/Model/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  Favorite Games
+//
+//  Created by Bruno Silva on 12/10/21.
+//
+
+import UIKit
+
+struct Game {
+    
+    // MARK: - Properties
+    var image: UIImage
+    var title: String
+}
